db6c2c44ffd2b055a71d67a83bb649159153631b
Arek2017 <agront@interia.pl>
2023-05-15 23:14:00 +0200

    Almost finished all views and applied first mechanics to play with two players on one phone

--- app/src/main/java/com/example/gra/Game.java
+++ app/src/main/java/com/example/gra/Game.java
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.gra.fragments.gameAI;
 import com.example.gra.fragments.gameOnPhone;
@@ -13,17 +16,37 @@ import com.example.gra.fragments.gameOnPhone;
 public class Game extends AppCompatActivity {
 
     int selectedMode;
+
+    TextView textView3;
+    TextView textView7;
+    TextView textView8;
+    Button button;
+    Button button6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        button = findViewById(R.id.button);
+        button6 = findViewById(R.id.button6);
+        textView3 = findViewById(R.id.textView3);
+        textView7 = findViewById(R.id.textView7);
+        textView8 = findViewById(R.id.textView8);
+
         selectedMode = (int)getIntent().getSerializableExtra("selectedMode");
         if(selectedMode==1){
             replaceFragment(new gameAI());
+            textView3.setText("Gra z komputerem");
         }
         else if(selectedMode==2){
             replaceFragment(new gameOnPhone());
+            textView3.setText("Gra ze znajomym");
         }
+
+        button6.setOnClickListener(v -> {
+            Intent intent = new Intent(Game.this, MainPage.class);
+            startActivity(intent);
+        });
     }
 
     private void replaceFragment(Fragment fragment) {
--- app/src/main/java/com/example/gra/fragments/gameOnPhone.java
+++ app/src/main/java/com/example/gra/fragments/gameOnPhone.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.gra.R;
 
@@ -18,7 +19,38 @@ public class gameOnPhone extends Fragment {
     Button button31;
     Button button32;
     Button button33;
+    Button button34;
+    Button button35;
+    Button button36;
+    Button button37;
+    Button button38;
+    Button button39;
 
+    boolean xTurn = true;
+    String[][] matrix = new String[3][3];
+    int ys = 0;
+
+    public void placeMark(Button button){
+        if(xTurn == true) {
+            button.setBackground(getResources().getDrawable(R.drawable.cross_shape));
+        }
+        else{
+            button.setBackground(getResources().getDrawable(R.drawable.circle_shape));
+        }
+        xTurn = !xTurn;
+        button.setEnabled(false);
+        gameTerminate();
+    }
+
+    public void gameTerminate(){
+        if((matrix[0][0]=="X" && matrix[0][1]=="X" && matrix[0][2]=="X")||(matrix[1][0]=="X" && matrix[1][1]=="X" && matrix[1][2]=="X")||(matrix[2][0]=="X" && matrix[2][1]=="X" && matrix[2][2]=="X")||(matrix[0][0]=="X" && matrix[1][0]=="X" && matrix[2][0]=="X")||(matrix[0][1]=="X" && matrix[1][1]=="X" && matrix[2][1]=="X")||(matrix[0][2]=="X" && matrix[1][2]=="X" && matrix[2][2]=="X")||(matrix[0][0]=="X" && matrix[1][1]=="X" && matrix[2][2]=="X")||(matrix[0][2]=="X" && matrix[1][1]=="X" && matrix[2][0]=="X")) {
+            Toast.makeText(getActivity(), "X wygrał", Toast.LENGTH_SHORT).show();
+        }
+        else if((matrix[0][0]=="O" && matrix[0][1]=="O" && matrix[0][2]=="O")||(matrix[1][0]=="O" && matrix[1][1]=="O" && matrix[1][2]=="O")||(matrix[2][0]=="O" && matrix[2][1]=="O" && matrix[2][2]=="O")||(matrix[0][0]=="O" && matrix[1][0]=="O" && matrix[2][0]=="O")||(matrix[0][1]=="O" && matrix[1][1]=="O" && matrix[2][1]=="O")||(matrix[0][2]=="O" && matrix[1][2]=="O" && matrix[2][2]=="O")||(matrix[0][0]=="O" && matrix[1][1]=="O" && matrix[2][2]=="O")||(matrix[0][2]=="O" && matrix[1][1]=="O" && matrix[2][0]=="O")) {
+            Toast.makeText(getContext(), "O wygrał", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 
     public gameOnPhone() {
 
@@ -43,19 +75,58 @@ public class gameOnPhone extends Fragment {
         button31 = getActivity().findViewById(R.id.button31);
         button32 = getActivity().findViewById(R.id.button32);
         button33 = getActivity().findViewById(R.id.button33);
+        button34 = getActivity().findViewById(R.id.button34);
+        button35 = getActivity().findViewById(R.id.button35);
+        button36 = getActivity().findViewById(R.id.button36);
+        button37 = getActivity().findViewById(R.id.button37);
+        button38 = getActivity().findViewById(R.id.button38);
+        button39 = getActivity().findViewById(R.id.button39);
 
-        button31.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                button31.setBackground(getResources().getDrawable(R.drawable.cross_shape));
-            }
+        //public void
+
+        button31.setOnClickListener(view31 -> {
+            matrix[0][0] = xTurn ? "X" : "O";
+            placeMark(button31);
         });
 
-        button32.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                button32.setBackground(getResources().getDrawable(R.drawable.circle_shape));
-            }
+        button32.setOnClickListener(view32 -> {
+            matrix[0][1] = xTurn ? "X" : "O";
+            placeMark(button32);
+        });
+
+        button33.setOnClickListener(view33 -> {
+            matrix[0][2] = xTurn ? "X" : "O";
+            placeMark(button33);
+        });
+
+        button34.setOnClickListener(view34 -> {
+            matrix[1][0] = xTurn ? "X" : "O";
+            placeMark(button34);
+        });
+
+        button35.setOnClickListener(view35 -> {
+            matrix[1][1] = xTurn ? "X" : "O";
+            placeMark(button35);
+        });
+
+        button36.setOnClickListener(view36 -> {
+            matrix[1][2] = xTurn ? "X" : "O";
+            placeMark(button36);
+        });
+
+        button37.setOnClickListener(view37 -> {
+            matrix[2][0] = xTurn ? "X" : "O";
+            placeMark(button37);
+        });
+
+        button38.setOnClickListener(view38 -> {
+            matrix[2][1] = xTurn ? "X" : "O";
+            placeMark(button38);
+        });
+
+        button39.setOnClickListener(view39 -> {
+            matrix[2][2] = xTurn ? "X" : "O";
+            placeMark(button39);
         });
     }
 
